fc9540fa6e8ccc7617c8a9ed6b7a67269f69c70f
Sébastien <pellangeon@gmail.com>
2024-01-26 15:47:03 +0100

    back update 1

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/year', function(req, res, next) {
-  const date = new Date().getUTCFullYear().toString()
+  const date = new Date().getFullYear().toString()
+  console.log('type of is:', typeof date)
   res.json(({year : date}))
 });
 module.exports = router;
