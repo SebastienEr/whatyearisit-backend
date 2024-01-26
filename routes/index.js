e6f744d4c780cedb1c4ded3e427afa7332543882
Sébastien <pellangeon@gmail.com>
2024-01-26 12:43:20 +0100

    jjjj

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/year', function(req, res, next) {
+  const date = new Date().getUTCFullYear()
+  res.json(({year : date}))
 });
-
-
-
-const express = require('express');
-const app = express();
-
-app.get('/year', (req, res) => {
-  const currentYear = new Date().getFullYear();
-  res.send(`Current year is: ${currentYear}`);
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-
-
-
-
-
-
-
-
 module.exports = router;
